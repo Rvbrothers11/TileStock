49c79353b58e1b600ac93e5d6dd061c64f360ed2
Vidit <viditbhati@gmail.com>
2026-07-04 16:20:17 +0530

    Initialized API tokens and extracted core DOM references

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 const API_KEY = 'd94ch4pr01qj2cibo1pgd94ch4pr01qj2cibo1q0';
+
+
+const searchBtn = document.getElementById('searchBtn');
+const tickerInput = document.getElementById('tickerInput');
+const dashboard = document.getElementById('dashboard');
+const welcomeState = document.getElementById('welcomeState');
+const historyPanel = document.getElementById('historyPanel');
+const portfolioPanel = document.getElementById('portfolioPanel');
+const historyList = document.getElementById('historyList');
